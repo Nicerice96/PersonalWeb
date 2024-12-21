c57d8ba7b1f34662b844cf15810f88fee598ff1c
Nicerice96 <100174834+Nicerice96@users.noreply.github.com>
2024-12-21 11:27:11 -0500

    we are officially reacting

---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,19 @@
+function Header(){
+
+    return(
+        <header>
+            <h1>LLaVA-OneVision</h1>
+            <ul>
+                <li>Try it!</li>
+                <li>Contact Development Team</li>
+                <li>About us</li>
+            </ul>
+
+        </header>
+
+    );
+
+
+}
+
+export default Header
