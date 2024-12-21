8a71ef9e0ccfddd1bb23e1c305351d5a30e9ce3c
Nicerice96 <100174834+Nicerice96@users.noreply.github.com>
2024-12-21 17:54:14 -0500

    updated some of the html, have to think about this...

--- my-react-app/src/Body.jsx
+++ my-react-app/src/Body.jsx
@@ -1,7 +1,6 @@
 function Body(){
     return (
         <body>
-
         </body>
     );
 }
---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer(){
+
+
+    return (
+        <footer>
+            <h1>this is a footer</h1>
+        </footer>
+
+    );
+
+
+
+
+}
+
+
+export default Footer
